7a87fd1b4a381bd8c6665f57328bb56513a3a490
jahid0001 <jahidhossain0213@gmail.com>
2023-02-06 23:36:16 +0600

    ASSIG-M-2

--- Assignmentofmodule-2.php
+++ Assignmentofmodule-2.php
@@ -2,7 +2,7 @@
 
 // Build a very simple commission-calculating calculator for HF consultancy Admission agents: tuition fee = (7000 to 20000 dollars above)
 
-$tuition_fee = 12000;
+$tuition_fee = 20000;
 
 
 $addcommission =($tuition_fee>= 20000)? "{$tuition_fee} to pay 25% commission" : (($tuition_fee>= 10000? "{$tuition_fee} to pay 20% commission" : 
