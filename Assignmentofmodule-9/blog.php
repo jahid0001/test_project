5fe542c854b93d2de787e6d011a15dec109591a4
jahid0001 <jahidhossain0213@gmail.com>
2023-04-04 21:30:38 +0600

    ASSIG-M-9

---
+++ Assignmentofmodule-9/blog.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Navigation bar design usign html & css & javascript</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <!-- font awesome cdn -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+        <header class="header-area">
+            <div class="header-container">
+                <div class="site-logo">
+                    <a href="http://localhost/web/index.php">Site<span>Logo</span></a>
+                </div>
+                <div class="mobile-nav">
+                    <i class="fas fa-bars"></i>
+                </div>
+                <div class="site-nav-menu">
+                    <ul class="primary-menu">
+                        <li><a href="http://localhost/web/index.php" class="active">Home</a></li>
+                        <li><a href="http://localhost/web/blog.php">Blog</a></li>
+                        <li><a href="http://localhost/web/contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+
+        <div class="container">
+        <div class="row">
+            <div class="header">
+                <h1>Our Blog</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Harum, accusamus qui sapiente numquam nesciunt </p>
+                    
+
+            </div>
+
+            <div class="content">
+                <div class="card">
+                    <img src="img/blogp.jpg">
+                    <h4>Lorem ipsum dolor sit amet</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Minima quod et tempore. Cum veniam similique deleniti voluptate,   
+                    </p>
+                    <a href="" class="button">Read More</a>
+                </div>
+
+                <div class="card">
+                    <img src="img/blogp.jpg">
+                    <h4>Lorem ipsum dolor sit amet</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Minima quod et tempore. Cum veniam similique deleniti voluptate,
+                    </p>
+                    <a href="" class="button">Read More</a>
+                </div>
+
+                <div class="card">
+                    <img src="img/blogp.jpg">
+                    <h4>Lorem ipsum dolor sit amet</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Minima quod et tempore. Cum veniam similique deleniti voluptate,
+                    </p>
+                    <a href="" class="button">Read More</a>
+                </div>
+
+               
+
+            </div>
+         
+        </div>
+    
+    
+    
+    </div>
+   
+  
+    
+</body>
+</html>
---
+++ Assignmentofmodule-9/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Navigation bar design usign html & css & javascript</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <!-- font awesome cdn -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="header-area">
+        <div class="header-container">
+            <div class="site-logo">
+                <a href="http://localhost/web/index.php">Site<span>Logo</span></a>
+            </div>
+            <div class="mobile-nav">
+                <i class="fas fa-bars"></i>
+            </div>
+            <div class="site-nav-menu">
+                <ul class="primary-menu">
+                    <li><a href="" class="active">Home</a></li>
+                    <li><a href="http://localhost/web/blog.php">Blog</a></li>
+                    <li><a href="http://localhost/web/contact.php">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+  
+    <div class="container">
+        <div class="row">
+            <div class="header">
+                <h1>Welcome To Our Website</h1>
+
+            </div>
+            <div class="header">
+                <h2 style="margin-top: 59px;">Our Blog Post</h2>
+
+            </div>
+
+
+            <div class="content">
+                <div class="card">
+                    <img src="img/blogp.jpg">
+                    <h4>Lorem ipsum dolor sit amet</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Minima quod et tempore. Cum veniam similique deleniti voluptate,   
+                    </p>
+                    <a href="http://localhost/web/blog.php" class="button">Read More</a>
+                </div>
+
+                <div class="card">
+                    <img src="img/blogp.jpg">
+                    <h4>Lorem ipsum dolor sit amet</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Minima quod et tempore. Cum veniam similique deleniti voluptate,
+                    </p>
+                    <a href="http://localhost/web/blog.php" class="button">Read More</a>
+                </div>
+
+                <div class="card">
+                    <img src="img/blogp.jpg">
+                    <h4>Lorem ipsum dolor sit amet</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Minima quod et tempore. Cum veniam similique deleniti voluptate,
+                    </p>
+                    <a href="http://localhost/web/blog.php" class="button">Read More</a>
+                </div>
+
+               
+
+            </div>
+
+
+
+        </div>
+    
+    
+    
+    </div>   
+  
+    <!-- <section id="hero-section"> </section> -->
+    
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="app.js"></script> -->
+</body>
+</html>
